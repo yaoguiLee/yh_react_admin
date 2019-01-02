d4a92b88498dd33a2b4dee16ab3e1dc84d4406a3
liyaohui <65102243@qq.com>
2019-01-02 23:24:46 +0800

    添加背景图片

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import { Switch, Route, Link } from 'react-router-dom'
 import PageA from './pages/PageA'
 import PageB from './pages/PageB'
+import login from './static/login.jpg'
+import './styles/app.css'
 class App extends Component {
   render() {
+    const loginImage = {background: `url(${login})` }
     return (
-      <div>
-        <h1>App</h1>
+      <div className="app_container">
+        <h1 style={{ color: '#fff'}}>App</h1>
         <ul>
           <li><Link to="/pagea">Page A</Link></li>
           <li><Link to="/pageb">Page B</Link></li>
