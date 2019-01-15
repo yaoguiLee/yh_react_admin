3359b1a23c6f411e9fa0c21fca3343477c0baf65
yhli831114 <yaohui.li@ubi001.com>
2019-01-15 17:28:48 +0800

    修改化天气图片的小问题

--- src/views/HomePage/components/Weather.js
+++ src/views/HomePage/components/Weather.js
@@ -3,7 +3,7 @@ const Weather = ({province, city, weather, png, temperature}) => {
   return (
     <div className="yh_weather_container">
       <div className="yh_weather_left">
-        <img src={png} alt="weather" width="80" height="80"/>
+        { png && <img src={png} alt="weather" width="80" height="80"/> }
         <p style={{margin: '0 auto'}}>{weather}</p>
       </div>
       <div className="yh_weather_right">
